b28523c33f727f436702e3c7f1ca70af2c73c1ad
SignKHE <papernote39@gmail.com>
2026-02-09 04:14:33 +0900

    인게임 UI 생성 추가 + 타이머 추가

--- Assets/ExorcistGame/Scenes/GameScene/GameSceneMng.cs
+++ Assets/ExorcistGame/Scenes/GameScene/GameSceneMng.cs
@@ -1,4 +1,7 @@
 using ExorcistGame.Spawn;
+using ExorcistGame.UI;
+using ExorcistGame.UI.InGameView;
+using R3;
 using UnityEngine;
 
 namespace ExorcistGame
@@ -7,12 +10,18 @@ namespace ExorcistGame
     {
         [SerializeField]
         private SpawnManager spawnManager;
+        [SerializeField]
+        private GameObject inGameViewPrefab;
 
-        private float _leftTime = 600f;
+        private GameTimer _gameTimer = new GameTimer(600.0f);
+
+        private InGameViewModel _viewModel;
         
         private async void Start()
         {
             Debug.Log("GameSceneMng Start");
+
+            CreateInGameUI();
 
             await Awaitable.WaitForSecondsAsync(3f);
             
@@ -20,16 +29,22 @@ namespace ExorcistGame
             
         }
 
+        private void CreateInGameUI()
+        {
+            _viewModel = new InGameViewModel(_gameTimer);
+            UIManager.Instance.CreateUI(inGameViewPrefab,_viewModel, UIManager.EUIType.Main);
+        }
+
         private async void GameLogic()
         {
             Debug.Log($"게임로직 시작");
             
-            while (_leftTime >= 0f)
+            while (_gameTimer.LeftTime.Value >= 0f)
             {
                 Spawn();
                 await Awaitable.WaitForSecondsAsync(1f);
                 Debug.Log($"카운트 다운");
-                _leftTime -= 1f;
+                _gameTimer.CountDown();
             }
         }
 
@@ -38,11 +53,11 @@ namespace ExorcistGame
             Debug.Log($"스폰 명령");
             int spawnCount = 0;
 
-            if (_leftTime > 480)
+            if (_gameTimer.LeftTime.Value > 480)
             {
                 spawnCount = 4;
             }
-            else if (_leftTime > 300)
+            else if (_gameTimer.LeftTime.Value > 300)
             {
                 spawnCount = 5;
             }
@@ -53,7 +68,21 @@ namespace ExorcistGame
             
             spawnManager.RequestSpawn( spawnCount , 5f);
         }
-        
-        
+
+        public class GameTimer : ITimer
+        {
+            public GameTimer(float leftTime)
+            {
+                LeftTime = new ReactiveProperty<float>();
+                LeftTime.Value = leftTime;
+            }
+
+            public void CountDown()
+            {
+                LeftTime.Value -= 1.0f;
+            }
+
+            public ReactiveProperty<float> LeftTime { get; }
+        }
     }
 }
