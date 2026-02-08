230d0367c5308d9e85d3d74f95714bd13adfcd34
SignKHE <papernote39@gmail.com>
2026-02-09 03:24:40 +0900

    스폰 타이머 로직 적용
    몬스터 스폰 갯수
    0~2분 - 4마리
    2~5분 - 5마리
    5분     - 6마리

--- Assets/ExorcistGame/Scenes/GameScene/GameSceneMng.cs
+++ Assets/ExorcistGame/Scenes/GameScene/GameSceneMng.cs
@@ -8,24 +8,51 @@ namespace ExorcistGame
         [SerializeField]
         private SpawnManager spawnManager;
 
+        private float _leftTime = 600f;
+        
         private async void Start()
         {
             Debug.Log("GameSceneMng Start");
 
             await Awaitable.WaitForSecondsAsync(3f);
-            spawnManager.RequestSpawn( 30, 10f);
-            await Awaitable.WaitForSecondsAsync(3f);
-            spawnManager.RequestSpawn(30, 10f);
-            await Awaitable.WaitForSecondsAsync(3f);
-            spawnManager.RequestSpawn( 30, 10f);
-            await Awaitable.WaitForSecondsAsync(3f);
-            spawnManager.RequestSpawn( 30, 10f);
-            await Awaitable.WaitForSecondsAsync(3f);
-            spawnManager.RequestSpawn( 30, 10f);
-            await Awaitable.WaitForSecondsAsync(3f);
-            spawnManager.RequestSpawn( 30, 10f);
+            
+            GameLogic();
+            
         }
-        
+
+        private async void GameLogic()
+        {
+            Debug.Log($"게임로직 시작");
+            
+            while (_leftTime >= 0f)
+            {
+                Spawn();
+                await Awaitable.WaitForSecondsAsync(1f);
+                Debug.Log($"카운트 다운");
+                _leftTime -= 1f;
+            }
+        }
+
+        private void Spawn()
+        {
+            Debug.Log($"스폰 명령");
+            int spawnCount = 0;
+
+            if (_leftTime > 480)
+            {
+                spawnCount = 4;
+            }
+            else if (_leftTime > 300)
+            {
+                spawnCount = 5;
+            }
+            else
+            {
+                spawnCount = 6;
+            }
+            
+            spawnManager.RequestSpawn( spawnCount , 5f);
+        }
         
         
     }
