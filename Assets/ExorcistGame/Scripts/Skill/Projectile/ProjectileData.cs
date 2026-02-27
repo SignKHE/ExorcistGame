faa1cc132a36783a9f56d0fc5728bddb60ce543a
SignKHE <papernote39@gmail.com>
2026-02-27 21:12:59 +0900

    투사체 충돌 처리 구현
    투사체의 충돌처리를 처리하는 ProjectileTriggerJob과
    충돌된 투사체의 데미지 처리를 처리하는 ProjectileHitJob으로 분리
    
    HitBuffer : 충돌 정보 버퍼

--- Assets/ExorcistGame/Scripts/Skill/Projectile/ProjectileData.cs
+++ Assets/ExorcistGame/Scripts/Skill/Projectile/ProjectileData.cs
@@ -6,6 +6,10 @@ namespace ExorcistGame.Skill
     public struct ProjectileData : IComponentData
     {
         /// <summary>
+        /// 투사체 데미지
+        /// </summary>
+        public float Damage;
+        /// <summary>
         /// 방향
         /// </summary>
         public float3 Direction;
@@ -25,12 +29,26 @@ namespace ExorcistGame.Skill
         /// 투사체 충돌 유형
         /// </summary>
         public EProjectileTriggerType TriggerType;
-        
+        /// <summary>
+        /// 투사체 생성 주체
+        /// </summary>
+        public Entity Instigator;
+
+        ProjectileData(float damage, float3 direction, float speed, float lifeTime, Entity instigator, EProjectileTriggerType triggerType = EProjectileTriggerType.SingleTarget)
+        {
+            Damage = damage;
+            Direction = direction;
+            Speed = speed;
+            LifeTime = lifeTime;
+            LeftLife = lifeTime;
+            TriggerType = triggerType;
+            Instigator = instigator;
+        }
     }
 
     public enum EProjectileTriggerType : byte
     {
-        Single,
-        Multi
+        SingleTarget,
+        MultiTarget
     }
 }
--- Assets/ExorcistGame/Scripts/Skill/Projectile/ProjectileHitSystem.cs
+++ Assets/ExorcistGame/Scripts/Skill/Projectile/ProjectileHitSystem.cs
@@ -12,19 +12,61 @@ namespace ExorcistGame.Skill
     [UpdateAfter(typeof(PhysicsSystemGroup))]
     public partial struct ProjectileHitSystem : ISystem
     {
+        public void OnCreate(ref SystemState state)
+        {
+            state.RequireForUpdate<EndSimulationEntityCommandBufferSystem.Singleton>();
+            state.RequireForUpdate<SimulationSingleton>();
+        }
+
         [BurstCompile]
         public void OnUpdate(ref SystemState state)
         {
+            var simulation = SystemAPI.GetSingleton<SimulationSingleton>();
             
+            var hitRecordECB = new EntityCommandBuffer(Allocator.TempJob);
+            
+            var projectileLookup = SystemAPI.GetComponentLookup<ProjectileData>(true);
+            var targetLookup = SystemAPI.GetComponentLookup<HPData>(true);
+            var playerLookup = SystemAPI.GetComponentLookup<PlayerTag>(true);
+            
+            var job = new ProjectileTriggerJob
+            {
+                ProjectileLookup = projectileLookup,
+                TargetLookup = targetLookup,
+                PlayerLookup = playerLookup,
+                ECB = hitRecordECB.AsParallelWriter()
+            };
+            
+            // 투사체 충돌 처리 실행
+            var hitJobHandle = job.Schedule(simulation, state.Dependency);
+            hitJobHandle.Complete();
+            // 투사체 충돌 처리가 완료된 다음 ECB 처리
+            hitRecordECB.Playback(state.EntityManager);
+            hitRecordECB.Dispose();
+
+            var ecbSingleton = SystemAPI.GetSingleton<EndSimulationEntityCommandBufferSystem.Singleton>();
+            var hitResolveEcb = ecbSingleton.CreateCommandBuffer(state.WorldUnmanaged).AsParallelWriter();
+            
+            var hitResolveJob = new ProjectileHitJob
+            {
+                ECB = hitResolveEcb
+            };
+            
+            // 충돌한 투사체의 데미지 처리 실행
+            state.Dependency = hitResolveJob.ScheduleParallel(state.Dependency);
         }
     }
     
+    /// <summary>
+    /// 투사체 충돌처리 구현
+    /// </summary>
     [BurstCompile]
     public struct ProjectileTriggerJob : ITriggerEventsJob
     {
         [ReadOnly] public ComponentLookup<ProjectileData> ProjectileLookup;
         [ReadOnly] public ComponentLookup<HPData> TargetLookup;
         [ReadOnly] public ComponentLookup<PlayerTag> PlayerLookup;
+        public EntityCommandBuffer.ParallelWriter ECB;
 
         public void Execute(TriggerEvent triggerEvent)
         {
@@ -49,8 +91,43 @@ namespace ExorcistGame.Skill
                 // 투사체와 타겟이 같은 편이 아닐때 충돌 판정
                 if (projectileIsPlayer != targetIsPlayer)
                 {
-                    // 타겟에게 데미지
+                    // 투사체에 Hit버퍼 생성.
+                    int sortKey = projectileEntity.Index;
+
+                    ECB.AddBuffer<HitBuffer>(sortKey, projectileEntity);
+                    ECB.AppendToBuffer(sortKey, projectileEntity, 
+                        new HitBuffer()
+                        {
+                            Target = targetEntity,
+                            DamageData = new DamageBufferElement
+                            {
+                                Value = ProjectileLookup[projectileEntity].Damage, 
+                                Instigator = ProjectileLookup[projectileEntity].Instigator
+                            }
+                        });
+                    
                 }
+            }
+        }
+    }
+    
+    /// <summary>
+    /// Hit 버퍼를 가진 투사체 엔치치의 데미지 처리를 구현
+    /// </summary>
+    [BurstCompile]
+    public partial struct ProjectileHitJob : IJobEntity
+    {
+        public EntityCommandBuffer.ParallelWriter ECB;
+
+        public void Execute(Entity projectileEntity, [EntityIndexInQuery] int sortKey, ref DynamicBuffer<HitBuffer> hits)
+        {
+            if (hits.Length > 0)
+            {
+                Entity targetEntity = hits[0].Target;
+
+                ECB.AddBuffer<DamageBufferElement>(sortKey, targetEntity);
+                ECB.AppendToBuffer(sortKey, targetEntity, hits[0].DamageData);
+                ECB.DestroyEntity(sortKey, projectileEntity);
             }
         }
     }
