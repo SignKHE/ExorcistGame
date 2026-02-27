c0d00529ebbbbcf19e3618e67907b855914dc4e7
SignKHE <papernote39@gmail.com>
2026-02-27 17:13:41 +0900

    Update ProjectileData.cs
    수명과 투사체 충돌 유형 정보 추가

--- Assets/ExorcistGame/Scripts/Skill/Projectile/ProjectileData.cs
+++ Assets/ExorcistGame/Scripts/Skill/Projectile/ProjectileData.cs
@@ -5,7 +5,32 @@ namespace ExorcistGame.Skill
 {
     public struct ProjectileData : IComponentData
     {
+        /// <summary>
+        /// 방향
+        /// </summary>
         public float3 Direction;
+        /// <summary>
+        /// 속도
+        /// </summary>
         public float Speed;
+        /// <summary>
+        /// 수명
+        /// </summary>
+        public float LifeTime;
+        /// <summary>
+        /// 남은 수명
+        /// </summary>
+        public float LeftLife;
+        /// <summary>
+        /// 투사체 충돌 유형
+        /// </summary>
+        public EProjectileTriggerType TriggerType;
+        
+    }
+
+    public enum EProjectileTriggerType : byte
+    {
+        Single,
+        Multi
     }
 }
