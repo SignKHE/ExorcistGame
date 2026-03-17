fbbe0003dc414d1132b356fe013f8de41f4942a6
SignKHE <papernote39@gmail.com>
2026-03-17 14:36:47 +0900

    Update ProjectileMovementSystem.cs
    BurstCompile 추가
    시스템 설명 추가

--- Assets/ExorcistGame/Scripts/Skill/Projectile/ProjectileMovementSystem.cs
+++ Assets/ExorcistGame/Scripts/Skill/Projectile/ProjectileMovementSystem.cs
@@ -5,16 +5,20 @@ using Unity.Transforms;
 
 namespace ExorcistGame.Skill
 {
+    /// <summary>
+    /// 움직임이 가능한 투사체의 움직임 구현 시스템
+    /// </summary>
     [BurstCompile]
     public partial struct ProjectileMovementSystem : ISystem
     {
+        [BurstCompile]
         public void OnUpdate(ref SystemState state)
         {
             float deltaTime = SystemAPI.Time.DeltaTime;
 
             foreach (var (movementData, transform, entity) 
                      in SystemAPI.Query<RefRO<MovementData>, RefRW<LocalTransform>>()
-                         .WithAll<ProjectileData>().WithEntityAccess() )
+                         .WithAll<ProjectileData>().WithEntityAccess())
             {
                 float3 velocity = movementData.ValueRO.Direction * movementData.ValueRO.Speed * deltaTime;
                 transform.ValueRW.Rotation = quaternion.LookRotationSafe(movementData.ValueRO.Direction, math.up());
